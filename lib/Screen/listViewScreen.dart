931cd171d10009bd7de4c4db47665b25e1f14532
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-08-25 16:09:02 +0800

    assign loaded json data to list view

--- lib/Screen/listViewScreen.dart
+++ lib/Screen/listViewScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:dashboard/Model/stateStats.dart';
 import 'package:flutter/material.dart';
 
 import '../Model/network.dart';
@@ -12,6 +13,8 @@ class ListViewScreen extends StatefulWidget {
 
 class _ListViewScreenState extends State<ListViewScreen> {
   List names = ['Zainal', 'Amir', 'Kamarul', 'Shafiq', 'Diana', 'Siti'];
+  late StatesStats stateStats;
+  List<Data> senaraiData = [];
 
   @override
   void initState() {
@@ -20,13 +23,16 @@ class _ListViewScreenState extends State<ListViewScreen> {
     loadStats();
   }
 
-  loadStats() async{
-    var url = 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=2020';
+  loadStats() async {
+    var url =
+        'https://datausa.io/api/data?drilldowns=State&measures=Population&year=2020';
     var res = await Network().getData(url);
 
     var body = jsonDecode(res.body);
-
-    print(body);
+    stateStats = StatesStats.fromJson(body);
+    setState(() {
+      senaraiData = stateStats.data;
+    });
   }
 
   @override
@@ -37,17 +43,22 @@ class _ListViewScreenState extends State<ListViewScreen> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
         drawer: CustomDrawer(),
-        body: ListView.builder(
-          itemCount: names.length,
-          itemBuilder: (BuildContext context, index) {
-            return ListTile(
-              leading: Icon(Icons.supervised_user_circle),
-              trailing: Icon(Icons.arrow_forward_ios),
-              title: Text(
-                names[index],
-              ),
-            );
-          },
-        ));
+        body: senaraiData.isEmpty
+            ? Center(child: const CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: senaraiData.length,
+                itemBuilder: (BuildContext context, index) {
+                  return ListTile(
+                    leading: Icon(Icons.supervised_user_circle),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    title: Text(
+                      senaraiData[index].state.toString(),
+                    ),
+                    subtitle:Text(
+                      senaraiData[index].population.toString(),
+                    ),
+                  );
+                },
+              ));
   }
 }
