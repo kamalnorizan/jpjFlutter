0d38b7112a4caa3d7ebc224b301a7413518d417e
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-08-25 15:25:20 +0800

    api request

--- lib/Screen/listViewScreen.dart
+++ lib/Screen/listViewScreen.dart
@@ -1,9 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+import '../Model/network.dart';
 import '../Widgets/customDrawer.dart';
 
-class ListViewScreen extends StatelessWidget {
+class ListViewScreen extends StatefulWidget {
+  @override
+  State<ListViewScreen> createState() => _ListViewScreenState();
+}
+
+class _ListViewScreenState extends State<ListViewScreen> {
   List names = ['Zainal', 'Amir', 'Kamarul', 'Shafiq', 'Diana', 'Siti'];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadStats();
+  }
+
+  loadStats() async{
+    var url = 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=2020';
+    var res = await Network().getData(url);
+
+    var body = jsonDecode(res.body);
+
+    print(body);
+  }
 
   @override
   Widget build(BuildContext context) {
